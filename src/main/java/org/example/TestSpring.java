29d5796bb8624c2c1432db574ca56a9f789c86c8
Miron <miron.yusupov04@gmail.com>
2023-05-08 17:48:42 +0300

    add JavaConfig

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,14 +1,14 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-
 
 //        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
 //        musicPlayer.playMusic();
