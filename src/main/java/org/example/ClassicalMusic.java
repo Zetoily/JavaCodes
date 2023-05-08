fcda00bf538151e5b382543ac6e659f0aff9faeb
Miron <miron.yusupov04@gmail.com>
2023-05-08 17:26:48 +0300

    add dependency

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -3,16 +3,19 @@ package org.example;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class ClassicalMusic implements Music{
-
+    @PostConstruct
     public void doMyInit(){
         System.out.println("Doing initialization...");
     }
+    @PreDestroy
     public void doMyDestroy(){
         System.out.println("Doing destroy...");
     }
